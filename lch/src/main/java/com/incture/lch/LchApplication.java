e3b0ed61e23a48c05f5d55e308261d97386944af
Urmil Sarit <Urmil Sarit@INC01459>
2021-03-22 09:44:14 +0530

    Added Swagger Configuration

--- lch/src/main/java/com/incture/lch/LchApplication.java
+++ lch/src/main/java/com/incture/lch/LchApplication.java
@@ -20,9 +20,9 @@ public class LchApplication {
 		SpringApplication.run(LchApplication.class, args);
 	}
 
-	@Bean
+	/*@Bean
 	   public Docket productApi() {
 	      return new Docket(DocumentationType.SWAGGER_2).select()
 	         .apis(RequestHandlerSelectors.basePackage("com.incture.lch")).build();
-	   }
+	   }*/
 }
