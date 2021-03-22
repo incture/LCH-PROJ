6e9415b4e748a399e9ddeaa7e46a650c0d6249d6
athulantony-1 <athul.antony@incture.com>
2021-03-22 15:04:26 +0530

    swaggerconfig

---
+++ lch/src/main/java/com/incture/lch/config/SwaggerConfiguration.java
@@ -0,0 +1,50 @@
+package com.incture.lch.config;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.web.method.annotation.AuthenticationPrincipalArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+
+@ComponentScan(basePackages = { "com.incture.*" })
+public class SwaggerConfiguration extends WebMvcConfigurationSupport {
+
+	@Bean
+	public Docket productApi() {
+		return new Docket(DocumentationType.SWAGGER_2).select().paths(regex("/.*")).build().apiInfo(metaData());
+
+	}
+
+	@Override
+	protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+
+		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
+
+	@Override
+	protected void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+		argumentResolvers.add(new AuthenticationPrincipalArgumentResolver());
+	}
+
+	private ApiInfo metaData() {
+		return new ApiInfoBuilder().title("LCH REST API DOCUMENTATION").description("\"\"").version("1.0.0")
+				.contact(new Contact("LCH - Incture Technologies", null, "")).build();
+	}
+}
